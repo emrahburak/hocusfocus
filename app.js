8f4e616b9c59b6cefa3df642fc0631d68082e646
emrahburak <gemrahburak@gmail.com>
2022-07-27 12:49:52 +0300

    commander plain is working

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 
 const sound = require('sound-play');
-const path = require('path')
+const commander = require('commander');
 
-const args = process.argv[process.argv.length -1]
+const path = require('path');
+const fs = require('fs');
 
-const filePath = path.join(__dirname,`${args}`);
 
-sound.play(filePath).then(res => console.log('DONE!'));
+// basic plain
+commander
+  .version('1.0.0', '-v, --version')
+  .usage('[OPTIONS]...')
+//   .option('-f, --flag', 'Detects if the flag is present.')
+  .option('-p, --path <value>', 'Overwriting value.', './audio/audio.mp3')
+  .parse(process.argv);
+
+const options = commander.opts();
+const absoloutePath = path.resolve(options.path);
+
+
+// chek some rules
+const flag = (options.flag ? 'Flag is present.' : 'Flag is not present.');
+var stats = fs.statSync(absoloutePath);
+
+
+//
+
+// console.log('Flag:', `${flag}`);
+// console.log('Custom:', `${options.path}`);
+// console.log(absoloutePath)
+// console.log(args)
+// console.log(argsAll)
+
+
+// const filePath = path.join(__dirname,`${options.path}`);
+stats.isFile() ? sound.play(absoloutePath).then(res => console.log('DONE!')) : console.log("opps")
+
 
 
 // console.log(process.argv[process.argv.length -1]);
