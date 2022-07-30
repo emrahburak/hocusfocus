e90dd383d6122467ad4be4e257342e47fa94e04a
emrahburak <gemrahburak@gmail.com>
2022-07-30 15:34:56 +0300

    workaraund on setInterval

--- app.js
+++ app.js
@@ -1,45 +1,54 @@
+const sound = require("sound-play");
+const commander = require("commander");
 
-const sound = require('sound-play');
-const commander = require('commander');
+const path = require("path");
+const fs = require("fs");
+const { time } = require("console");
 
-const path = require('path');
-const fs = require('fs');
-
+const audioPath = "./audio/audio.mp3";
+const defaultDuration = 5000;
 
 // basic plain
 commander
-  .version('1.0.0', '-v, --version')
-  .usage('[OPTIONS]...')
-//   .option('-f, --flag', 'Detects if the flag is present.')
-  .option('-p, --path <value>', 'Overwriting value.', './audio/audio.mp3')
+  .version("1.0.0", "-v, --version")
+  .usage("[OPTIONS]...")
+  //   .option('-f, --flag', 'Detects if the flag is present.')
+  .option("-d, --duration <value>", "set time interval", defaultDuration)
+  .option("-p, --path <value>", "Overwriting value.")
   .parse(process.argv);
 
 const options = commander.opts();
-const absoloutePath = path.resolve(options.path);
 
+const absoloutePath = options.path ? options.path : audioPath;
+const duration = options.duration;
 
 // chek some rules
-const flag = (options.flag ? 'Flag is present.' : 'Flag is not present.');
-var stats = fs.statSync(absoloutePath);
-
-
-//
-
-// console.log('Flag:', `${flag}`);
-// console.log('Custom:', `${options.path}`);
-// console.log(absoloutePath)
-// console.log(args)
-// console.log(argsAll)
-
+// const flag = (options.flag ? 'Flag is present.' : 'Flag is not present.');
 
 // const filePath = path.join(__dirname,`${options.path}`);
-stats.isFile() && sound.play(absoloutePath).then(res => console.log('DONE!')) 
 
+let run = (file) => {
+  let myFile = path.resolve(file);
+  var stats = fs.statSync(myFile).isFile();
+  try {
+    if (stats) {
+      return sound.play(myFile).then(() => console.log("Done"));
+    }
+  } catch (error) {
+    return new Error("Can't open file. Path is not correct");
+  }
+};
 
+let timer = () => {
+  let n = duration / 1000;
+  while (n >= 0) {
+    setInterval(() => {
+      console.log(n);
+    }, 1000);
+    n = n - 1;
+  }
+};
 
-// console.log(process.argv[process.argv.length -1]);
+timer();
 
-
-
-
-
+// run(absoloutePath)
