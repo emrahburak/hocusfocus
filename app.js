fc32686bd38d37a535439ea2af9c796f7548148f
emrahburak <gemrahburak@gmail.com>
2022-09-19 00:31:56 +0300

    new Date with generator function

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const Print = require("one-line-print");
 
 const path = require("path");
 const fs = require("fs");
-const { callbackify } = require("util");
 
 const audioPath = "./audio/audio.mp3";
 const defaultDuration = 1500;
@@ -20,51 +19,66 @@ commander
 
 const options = commander.opts();
 
-// chek some rules
 
+// chek some rules
 const absoloutePath = options.path ? options.path : audioPath;
 const duration = options.duration ? options.duration : defaultDuration;
 // console.log(duration);
 
-// convert seconds to time format
-
-// const toTime = (secs) => {
-//   const sign = secs < 0;
-//   const hhmmss = new Date(Math.abs(secs) * 1000).toISOString().substr(11, 8);
-//   return sign ? '-' + hhmmss: hhmmss;
-// };
 
 // utils
+// convert seconds to time format
 const toTime = function* (secs) {
   yield new Date(Math.abs(secs) * 1000).toISOString().substr(11, 8);
 };
 
-const iteration = (iteretor, val) => {
-  return iteretor(val).next().value;
+//only generator functions
+const iteration = (iteretor, val, fn) => {
+  return iteretor(val, fn).next().value;
 };
 
-const result = function(){
-  return false;
+
+//for test
+// const testCallback = function (value) {
+//   console.log("this is", value);
+// };
+
+
+// function provider
+const queue = [];
+
+
+// load function to function provider
+const load = (fn, arg) => {
+  queue.push([fn, arg]);
 };
 
-console.log(result());
 
-const counter = function* (n) {
-  let timer = yield setInterval(() => {
-    let getTime = iteration(toTime, n);
-    // let getTime = toTime(n).next().value;
+// dump and run  functions  from function provider
+const dump = () => {
+  while (queue.length) {
+    let [fn, arg] = queue.shift();
+    fn(arg);
+  }
+};
+
+
+// counter and timer
+const counter = function (n_duration, callback) {
+  let countdownTimer = setInterval(() => {
+    let getTime = iteration(toTime, n_duration);
     Print.line(`${getTime}`);
-    n = n - 1;
-    if (n < 0) {
-      clearInterval(counter);
-
+    n_duration--;
+    if (n_duration < 0) {
+      clearInterval(countdownTimer);
+      callback();
     }
   }, 1000);
 };
 
 
-// action
-const myRun = (file, time_s) => {
+// action run
+const run = (file, time_s) => {
   return new Promise((resolve, reject) => {
     // check path & file
     let myFile = path.resolve(file);
@@ -72,32 +86,21 @@ const myRun = (file, time_s) => {
 
     if (stats) {
       Print.newLine("Pomodoro");
-      let n = time_s;
+
+      //before countdown result payload
+      // load(testCallback, "testCallback");
+      load(resolve, myFile);
 
       // start countdown
-      let result = iteration(counter,n);
-
-      if(!result){
-        resolve(myFile);
-      }
-      
-      // let counter = setInterval(() => {
-      //   let getTime = toTime(n);
-      //   Print.line(`${getTime}`);
-      //   n = n - 1;
-      //   if (n < 0) {
-      //     resolve(myFile)
-      //     clearInterval(counter)
-      //   }
-      // },1000);
-    } else {
+      counter(time_s, dump);
+      } else {
       let result = new Error("Cant open file. Path is not corret");
       reject(result);
     }
   });
 };
 
-myRun(absoloutePath, duration)
+run(absoloutePath, duration)
   .then((res) => sound.play(res))
   .then(() => Print.newLine("Done"))
   .catch((err) => console.log(err.message));
--- timer.js
+++ timer.js
@@ -1,55 +1,85 @@
 // const co = require('co');
 
-const iteration  =  (iteretor,val) => {
-    return iteretor(val).next().value;
-}
-
-const toTime = function*(secs) {
-        yield new Date(Math.abs(secs) * 1000).toISOString().substr(11, 8);
+// utils
+const iteration = (iteretor, val) => {
+  return iteretor(val).next().value;
 };
 
-
-const boolean = function(){
-    return true
-}
-
-
-
-
-let counter = function(n,callback){
-    let timer =  setInterval(()=>{
-        let getTime = iteration(toTime,n)
-        // let getTime = toTime(n).next().value;
-        console.log(getTime);
-        n = n - 1;
-        if(n <= 0)
-            clearInterval(timer)
-            callback(true);
-    },1000);
+const toTime = function* (secs) {
+  yield new Date(Math.abs(secs) * 1000).toISOString().substr(11, 8);
 };
 
+const testCallback = function (value) {
+  console.log("this is", value);
+};
 
-var flag = true;
+const queue = [];
 
-const step1 = (n) => setInterval(()=>{
-    let getTime = iteration(toTime,n)
-    console.log(getTime);
-    n = n - 1;
-},1000)
+const load = (fn, arg) => {
+  queue.push([fn, arg]);
+};
 
-step1(5);
+const dump = () => {
+  while (queue.length) {
+    let [fn, arg] = queue.shift();
+    fn(arg);
+  }
+};
+
+const counter = (n, callback) => {
+  var timer = setInterval(function () {
+    let getTime = iteration(toTime, n);
+    // let getTime = toTime(n).next().value;
+    console.log(getTime, n);
+    n--;
+    if (n < 0) {
+      clearInterval(timer);
+      callback();
+    }
+  }, 1000);
+};
 
 const run = () => {
-    return new Promise((resolve,reject) => {
-        let result = counter(5,boolean)
-        if(result){
-            resolve(result);
-        }else{
-            reject("Some thing went wrong");
-        }
-    });
+  return new Promise((resolve, reject) => {
+    load(testCallback, "callback");
+    load(resolve, "myfile");
+    if (true) {
+      counter(5, dump);
+    } else {
+      reject("Some thing went wrong");
+    }
+  });
 };
 
-// run().then(res => console.log(res)).catch(err => console.log(err));
+run()
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
+// function rollDice(i, callback) {
+//   var test = setInterval(function () {
+//     let getTime = iteration(toTime, i);
+//     console.log(getTime, i);
+//     i--;
+//     // var value = Math.floor((Math.random() * 6) + 1);
+//     if (i < 1) {
+//       clearInterval(test);
+//       callback(i);
+//     }
+//   }, 1000);
+// }
+
+// counter(4, callable);
+// var sayi = Math.floor(Math.random() * 25 + 5);
+// rollDice(callable);
+
+// var flag = true;
+
+// const step1 = (n) => setInterval(()=>{
+//     let getTime = iteration(toTime,n)
+//     console.log(getTime);
+//     n = n - 1;
+// },1000)
+
+// step1(5);
 
 // counter(5,boolean);;
